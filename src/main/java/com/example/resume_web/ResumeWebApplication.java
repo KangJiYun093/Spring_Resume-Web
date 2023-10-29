73e4146ead43724445c35184e43a15c05960fa35
KangJiYun093 <regina091373@gmail.com>
2023-10-29 21:04:43 +0900

    초기설정

---
+++ src/main/java/com/example/resume_web/ResumeWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.resume_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resume_web/ResumeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resume_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
