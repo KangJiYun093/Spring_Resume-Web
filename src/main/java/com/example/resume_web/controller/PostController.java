4576f62d1eb610cf1085afc36f13876828b5cc19
dev-hyunjin <disappointed123419@gmail.com>
2023-12-02 23:25:43 +0900

    리스트 띄움

--- src/main/java/com/example/resume_web/controller/PostController.java
+++ src/main/java/com/example/resume_web/controller/PostController.java
@@ -1,21 +1,27 @@
 package com.example.resume_web.controller;
 
+import com.example.resume_web.dto.PostDto;
 import com.example.resume_web.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/post/*")
 @RequiredArgsConstructor
 public class PostController {
 
-//    private final PostService postService;
+    private final PostService postService;
 
     @GetMapping("post-list2")
-    public String postList() {
+    public String postList(Model model) {
+        List<PostDto> postLists = postService.getPostList();
 
+        model.addAttribute("posts", postLists);
 
         return "view/post-list2";
     }
