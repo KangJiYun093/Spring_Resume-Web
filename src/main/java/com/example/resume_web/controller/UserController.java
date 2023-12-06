b390c36e712cff5da2ffd013c2128a4f98f83cbe
KangJiYun093 <regina091373@gmail.com>
2023-12-06 22:09:27 +0900

    Revert "정리"
    This reverts commit 726d0b7bfd1635d6e610b3b25d89cfbd7d340ca4.

--- src/main/java/com/example/resume_web/controller/UserController.java
+++ src/main/java/com/example/resume_web/controller/UserController.java
@@ -1,55 +1,55 @@
-//package com.example.resume_web.controller;
-//
-//import com.example.resume_web.dto.UserDto;
-//import com.example.resume_web.service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.view.RedirectView;
-//
-//import javax.servlet.http.HttpSession;
-//
-//@Controller
-//@RequestMapping("/user/*")
-//@RequiredArgsConstructor
-//public class UserController {
-//
-//    private final UserService userService;
-//
-//    @GetMapping("login")
-//    public String login() {
-//        return "user/userLoginForm";
-//    }
-//
-//    @PostMapping("login")
-//    public RedirectView login(String userEmail, String userPassword, HttpSession session) {
-//        UserDto userDto = userService.login(userEmail, userPassword);
-//
-//        if(userDto != null) {
-//            session.setAttribute("member", userDto);
-//
-//            return new RedirectView("/post/post-list");
-//        }
-//
-//        return new RedirectView("/login?result=fail");
-//    }
-//
-//    @GetMapping("join")
-//    public String join() {
-//        return "user/userSignIn";
-//    }
-//
-//    @PostMapping("join")
-//    public RedirectView join(UserDto userDto) {
-//        userService.join(userDto);
-//
-//        return new RedirectView("/user/login");
-//    }
-//
-//    @GetMapping("edit")
-//    public String edit() {
-//        return "user/userEditForm";
-//    }
-//}
+package com.example.resume_web.controller;
+
+import com.example.resume_web.dto.UserDto;
+import com.example.resume_web.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/*")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("login")
+    public String login() {
+        return "user/userLoginForm";
+    }
+
+    @PostMapping("login")
+    public RedirectView login(String userEmail, String userPassword, HttpSession session) {
+        UserDto userDto = userService.login(userEmail, userPassword);
+
+        if(userDto != null) {
+            session.setAttribute("member", userDto);
+
+            return new RedirectView("/post/post-list");
+        }
+
+        return new RedirectView("/login?result=fail");
+    }
+
+    @GetMapping("join")
+    public String join() {
+        return "user/userSignIn";
+    }
+
+    @PostMapping("join")
+    public RedirectView join(UserDto userDto) {
+        userService.join(userDto);
+
+        return new RedirectView("/user/login");
+    }
+
+    @GetMapping("edit")
+    public String edit() {
+        return "user/userEditForm";
+    }
+}
